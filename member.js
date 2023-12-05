3ed2bf7c0fb45b311033221f227a47c9ac2a096f
ClarkTsai521 <58495879+ClarkTsai521@users.noreply.github.com>
2023-12-05 09:25:55 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,35 +1,10 @@
 function SkillsMember() {
-    var _this = this;
-
-    this.init = function () {
-        _this.initSkills();
-    };
-
-    this.initSkills = function () {
-        if ($('.skills').length) {
-            $('.skills').each(function () {
-                var $this = $(this);
-                var $skills = $this.find('.skill');
-                var $progressBar = $this.find('.progress-bar');
-                var $progressBarValue = $this.find('.progress-bar-value');
-                var $progressBarLabel = $this.find('.progress-bar-label');
-                var $progressBarValueLabel = $this.find('.progress-bar-value-label');
-                var $progressBarValueLabelValue = $this.find('.progress-bar-value-label-value');
-
-                $progressBarValueLabelValue.text($progressBarValue.val() + '%');
-
-                $progressBar.slider({
-                    min: 0,
-                    max: 100,
-                    value: $progressBarValue.val(),
-                    range: 'min',
-                    animate: true,
-                    slide: function (event, ui) {
-                        $progressBarValue.val(ui.value);
-                        $progressBarValueLabelValue.text(ui.value + '%');
-                    }
-                });
-            });
-        }
-    };
+  this.skills = [];
+  this.addSkill = function (skill) {
+    this.skills.push(skill);
+  };
+  this.removeSkill = function (skill) {
+    const index = this.skills.indexOf(skill);
+    this.skills.splice(index, 1);
+  };
 }
